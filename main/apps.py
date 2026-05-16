1616f0929b7efded4db995357f00e5a4c14b509f
Manas Mayank <mayank.manas.2801@gmail.com>
2026-05-16 20:22:51 +0530

    Auto create admin

--- main/apps.py
+++ main/apps.py
@@ -4,3 +4,16 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        try:
+            from django.contrib.auth.models import User
+
+            if not User.objects.filter(username='manas28').exists():
+                User.objects.create_superuser(
+                    'manas28',
+                    'mayank.manas.28012gmail.com',
+                    'sanam@82'
+                )
+        except:
+            pass
