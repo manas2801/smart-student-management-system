a00f19daadd14a69c62448ac989a54f3d3cc72ee
Manas Mayank <mayank.manas.2801@gmail.com>
2026-05-18 15:36:37 +0530

    Added Django testing

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Student
+
+
+class StudentModelTest(TestCase):
+
+    def test_student_creation(self):
+
+        student = Student.objects.create(
+
+            name="Manas",
+            email="mayank.manas.2801@gmail.com",
+            course="BCA",
+            phone="8709791448",
+            address="Muzaffarpur, Bihar"
+        )
+
+        self.assertEqual(student.name, "Manas")
+
+
+class HomePageTest(TestCase):
+
+    def test_homepage_status_code(self):
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
