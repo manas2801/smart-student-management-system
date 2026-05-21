9c82322257edefa4259eb21911224573a8bf8791
Manas Mayank <mayank.manas.2801@gmail.com>
2026-05-21 09:15:30 +0530

    Final Cloudinary fix

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,9 +105,9 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # MEDIA FILES
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
 # CLOUDINARY
 CLOUDINARY_STORAGE = {
